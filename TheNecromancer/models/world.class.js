ef6bd75a86f8ed2951a206b9aa913ee806a45263
Kim Burmester <Kim.Burmester@hotmail.de>
2025-11-15 03:32:14 +0100

    prepared the code for the keyboard saving for the movements

--- TheNecromancer/models/world.class.js
+++ TheNecromancer/models/world.class.js
@@ -1,20 +1,22 @@
 class World{
     character = new Character();
-    enemies = [new Enemy(),new Enemy(),new Enemy()];
+    enemies = [new Enemy(),new Enemy(),new Enemy(),new Enemy(),new Enemy(),new Enemy()];
     background = [new Background()]
     moon = [new Moon()]
     hill = [new Hills()]
     grave = [new Grave()]
     fence = [new Fence()]
     street = [new Street()]
-    clouds = [new Cloud()];
+    clouds = [];
     canvas;
     ctx;
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.createClouds();
         this.draw();
     }
+
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.addObjectsToMap(this.background);
@@ -34,5 +36,13 @@ class World{
     }
     addToMap(mo){
         this.ctx.drawImage(mo.img, mo.positionX, mo.positionY, mo.width, mo.height);
+    }
+    createClouds() {
+        let numberOfClouds = 5;
+        for (let i = 0; i < numberOfClouds; i++) {
+            let cloud = new Cloud();
+            cloud.positionX = i * (cloud.width - 10);
+            this.clouds.push(cloud);
+        }
     }
 }
