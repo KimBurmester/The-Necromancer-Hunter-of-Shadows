24b2b42c2eb88194befc0ae500c571e015cf5ed2
Kim Burmester <Kim.Burmester@hotmail.de>
2025-11-19 05:45:55 +0100

    Implemented another draw method that represents drawing a hitbox in order to implement a collision system later on.

--- TheNecromancer/models/world.class.js
+++ TheNecromancer/models/world.class.js
@@ -67,24 +67,35 @@ class World{
     setWorld(){
         this.character.world = this;
         this.enemies.forEach(enemy => {
-        enemy.world = this;
-    });
-    if (this.endboss) {
-        this.endboss.world = this;
-        console.log('✅ Endboss hat World-Referenz erhalten');
+            enemy.world = this;
+        });
+        if (this.endboss) {
+            this.endboss.world = this;
+            console.log('✅ Endboss hat World-Referenz erhalten');
+        }
     }
-    }
-    
 
     addObjectsToMap(objects){
         objects.forEach(o => this.addToMap(o));
+    }
+
+    isCharacter(mo) {
+        return mo instanceof Character;
+    }
+
+    isEnemy(mo) {
+        return mo instanceof Enemy;
+    }
+
+    isEndboss(mo) {
+        return mo instanceof Endboss;
     }
 
     addToMap(mo){
         if(mo.otherDirection){
             this.flipImageBack(mo);
         } else {
-            this.ctx.drawImage(mo.img, mo.positionX, mo.positionY, mo.width, mo.height);
+            this.drawFrameModel(mo);
         }
     }
 
@@ -93,6 +104,23 @@ class World{
         this.ctx.translate(mo.positionX + mo.width / 2, mo.positionY + mo.height / 2);
         this.ctx.scale(-1, 1);
         this.ctx.drawImage(mo.img, -mo.width / 2, -mo.height / 2, mo.width, mo.height);
+        
+        this.ctx.scale(-1, 1);
+        
+        if(this.isCharacter(mo)){
+            this.ctx.strokeStyle = 'red';
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeRect(-mo.width/2 + 50, -mo.height/2 + 50, mo.width - 120, mo.height - 85);
+        } else if(this.isEnemy(mo)){
+            this.ctx.strokeStyle = 'red';
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeRect(-mo.width/2 + 70, -mo.height/2 + 25, mo.width - 135, mo.height - 85);
+        } else if(this.isEndboss(mo)){
+            this.ctx.strokeStyle = 'red';
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeRect(-mo.width/2 + 150, -mo.height/2 + 120, mo.width - 290, mo.height - 205);
+        }
+        
         this.ctx.restore();
     }
 
@@ -101,5 +129,27 @@ class World{
             cloud.positionX = i * (cloud.width - 10);
         });
     }
-}
 
+    drawFrameModel(mo){
+        this.ctx.drawImage(mo.img, mo.positionX, mo.positionY, mo.width, mo.height);
+        if(this.isCharacter(mo)){
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeStyle = 'red';
+            this.ctx.strokeRect(mo.positionX + 50, mo.positionY + 50, mo.width - 120, mo.height - 85);
+            this.ctx.stroke();
+        } else if(this.isEnemy(mo)){
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeStyle = 'red';
+            this.ctx.strokeRect(mo.positionX + 70, mo.positionY + 25, mo.width - 135, mo.height - 85);
+            this.ctx.stroke();
+        } else if(this.isEndboss(mo)){
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeStyle = 'red';
+            this.ctx.strokeRect(mo.positionX + 150, mo.positionY + 120, mo.width - 290, mo.height - 205);
+            this.ctx.stroke();
+        }
+    }
+}
