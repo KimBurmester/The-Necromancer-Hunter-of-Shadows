dfc7cd4c5049d887130dcd0dedd63fdabece027e
Kim Burmester <Kim.Burmester@hotmail.de>
2025-11-20 02:25:33 +0100

    change the enemies.

--- TheNecromancer/level/level_1.js
+++ TheNecromancer/level/level_1.js
@@ -2,10 +2,7 @@ const level1 = new Level(
 [
     new Enemy(),
     new Enemy(),
-    new Enemy(),
-    new Enemy(),
-    new Enemy(),
-    new Enemy(),
+    new Enemy()
 ],
 [
     new Background()
